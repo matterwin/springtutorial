2d9a5c47759b1157ebea0d19c1cd9ef6a1b42215
matterwin <matthewderwin23@gmail.com>
2024-03-29 00:50:48 -0500

    h2 db version

--- runnerz/runnerz/src/main/java/dev/m3ttwin/runnerz/RunnerzApplication.java
+++ runnerz/runnerz/src/main/java/dev/m3ttwin/runnerz/RunnerzApplication.java
@@ -21,18 +21,4 @@ public class RunnerzApplication {
 		SpringApplication.run(RunnerzApplication.class, args);
 	}
 
-  @Bean
-  public CommandLineRunner runner() {
-    return args -> {
-      Run run = new Run(
-        1,
-        "First Run",
-        LocalDateTime.now(),
-        LocalDateTime.now().plus(1, ChronoUnit.HOURS),
-        5,
-        Location.OUTDOOR
-      );
-      log.info("Run: " + run);
-    };
-  }
 }
---
+++ runnerz/runnerz/src/main/java/dev/m3ttwin/runnerz/run/Runs.java
@@ -0,0 +1,6 @@
+package dev.m3ttwin.runnerz.run;
+
+import java.util.List;
+
+public record Runs(List<Run> runs) {
+}
